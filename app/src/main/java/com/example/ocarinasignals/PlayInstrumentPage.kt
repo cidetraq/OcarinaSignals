f3b4ff45119d8a1a264e5a7e1a53d142a1b72086
Nicholas Randall <nicholas.randall.contractor@hilton.com>
2022-09-02 15:34:23 -0500

    state passing

--- app/src/main/java/com/example/ocarinasignals/PlayInstrumentPage.kt
+++ app/src/main/java/com/example/ocarinasignals/PlayInstrumentPage.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -16,12 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun Ocarina(instrumentName: String){
+fun mainScreen(instrumentName: String, songSequences: Map<String,String> = mapOf<String,String>()){
     var scrollstate = ScrollState(0)
     Column(modifier=Modifier.verticalScroll(scrollstate)) {
         var playedNote by rememberSaveable { mutableStateOf("") }
         var playedSequence by rememberSaveable { mutableStateOf("") }
-        var songSequences by rememberSaveable { mutableStateOf(mutableMapOf<String, String>("C Major Triad" to "CEG", "The Lick" to "DEFGDECD")) }
         var isMatch by rememberSaveable { mutableStateOf(false) }
         Text(text = "Play $instrumentName")
         Text(text = "Note $playedNote played")
@@ -100,38 +97,51 @@ fun Ocarina(instrumentName: String){
                 }
             }
         }
-
-
-        var newSequenceName by rememberSaveable { mutableStateOf("") }
-        var newSequence by rememberSaveable { mutableStateOf("") }
-
-        fun submitNewSequence(){
-            songSequences[newSequenceName] = newSequence
-        }
-        Column(modifier= Modifier
-            .padding(20.dp)
-            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally){
-            TextField(
-                value = newSequenceName,
-                onValueChange = { newSequenceName = it },
-                label = { Text("New Sequence Name") },
-                singleLine = true, modifier=Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.size(20.dp))
-            TextField(
-                value = newSequence,
-                onValueChange = { newSequence = it },
-                label = { Text("New Sequence of Notes") },
-                singleLine = true, modifier=Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.size(20.dp))
-            Button(onClick = { submitNewSequence() }, modifier=Modifier.fillMaxWidth(.5f)) {
-                Text(text = "Submit New Sequence")
-            }
-        }
-
-
-
     }
 
+}
+
+@Composable
+fun SequenceEditor(updateSongSequences: (String,String)-> Unit ){
+    var newSequenceName by rememberSaveable { mutableStateOf("") }
+    var newSequence by rememberSaveable { mutableStateOf("") }
+    Column(modifier= Modifier
+        .padding(20.dp)
+        .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally){
+        TextField(
+            value = newSequenceName,
+            onValueChange = { newSequenceName = it },
+            label = { Text("New Sequence Name") },
+            singleLine = true, modifier=Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+        TextField(
+            value = newSequence,
+            onValueChange = { newSequence = it },
+            label = { Text("New Sequence of Notes") },
+            singleLine = true, modifier=Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+        Button(onClick = { updateSongSequences(newSequenceName,newSequence) }, modifier=Modifier.fillMaxWidth(.5f)) {
+            Text(text = "Submit New Sequence")
+        }
+    }
+}
+
+@Composable
+fun Ocarina(instrumentName: String){
+    var songSequences by rememberSaveable { mutableStateOf(mutableMapOf<String, String>("C Major Triad" to "CEG", "The Lick" to "DEFGDECD")) }
+    Scaffold(
+        drawerContent = {
+            Text("Drawer title", modifier = Modifier.padding(16.dp))
+            Divider()
+            Text("Main view", modifier = Modifier.padding(16.dp))
+            Text("Sequences", modifier = Modifier.padding(16.dp))
+        }
+    ) {
+        Column() {
+            mainScreen(instrumentName, songSequences)
+            SequenceEditor() { newSequenceName: String, newSequence: String -> songSequences[newSequenceName] = newSequence }
+        }
+       }
 }
