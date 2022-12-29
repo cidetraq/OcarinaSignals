7344e7a6b359c62e9a1342bb1a9bb4490d81826f
Nicholas Randall <nicholas.randall.contractor@hilton.com>
2022-12-29 09:59:13 -0600

    configure base amplify

--- app/src/main/java/com/example/ocarinasignals/MainActivity.kt
+++ app/src/main/java/com/example/ocarinasignals/MainActivity.kt
@@ -17,10 +17,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OcarinaSignalsTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Ocarina("ocarina")
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    OcarinaSignalsTheme {
+        Greeting("Android")
     }
 }
